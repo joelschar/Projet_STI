f0d2ad82843df6a6495811e2f5113c68b446b193
zutt <yannlederrey@gmail.com>
2018-10-01 13:43:59 +0200

    bordel

--- site/login.php
+++ site/login.php
@@ -29,14 +29,20 @@
 </head>
 <body>
 
+    <?php
+    if( $_GET["username"] || $_GET["pass"])
+    {
+    echo "Welcome: ". $_GET['username']. "<br />";
+    }
+    ?>
+
 	<div class="limiter">
 		<div class="container-login100" style="background-image: url('images/bg-01.jpg');">
 			<div class="wrap-login100 p-t-30 p-b-50">
 				<span class="login100-form-title p-b-41">
 					Account Login
 				</span>
-				<form class="login100-form validate-form p-b-33 p-t-5">
-
+				<form class="login100-form validate-form p-b-33 p-t-5" action="#" method="get">
 					<div class="wrap-input100 validate-input" data-validate = "Enter username">
 						<input class="input100" type="text" name="username" placeholder="User name">
 						<span class="focus-input100" data-placeholder="&#xe82a;"></span>
