d969f9ed71d58c081cff9058c3f8d63479be6036
Joel Schär <joel.schar@heig-vd.ch>
2019-01-11 13:58:20 +0100

    script

---
+++ start.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+docker rm -f sti_project
+
+docker run -ti -v "$PWD/site":/usr/share/nginx/ -d -p 8080:80 --name sti_project --hostname sti arubinst/sti:project2018
+docker exec -u root sti_project service nginx start
+docker exec -u root sti_project service php5-fpm start
+
+chmod -R 777 site/databases
